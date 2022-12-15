dc055d09a2b3f8ba361c6462ab1afa50489e63f1
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-15 22:56:50 +0530

    Add partcles to hero section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
+import Bubbles from "./scripts/particles";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+Bubbles();
